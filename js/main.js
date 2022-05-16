9e056bbd6413a8287ab139219e9eca5ad653da5a
sonelin09 <sonelin0309@gmail.com>
2022-05-16 15:07:28 +0800

    加入playRound函式比較玩家和電腦的輸贏

--- js/main.js
+++ js/main.js
@@ -1,8 +1,43 @@
-function computerPlay(){
+function computerPlay() {
     let onetwothree = Math.round(Math.random()*2);
     const pst = ["paper", "scissors", "rock"];
     let comPlay = pst[onetwothree];
-    console.log(comPlay);
+    return comPlay;
 }
 
-computerPlay();
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+
+function playRound(playerSelection, computerSelection) {
+    let outcome;
+    if (playerSelection === computerSelection){
+        outcome = "平手";
+    }else if (playerSelection === "paper"){
+        if (computerSelection === "scissors"){
+            outcome = "你輸惹";
+        }else{
+            outcome = "你贏了";
+        }
+    }else if (playerSelection === "scissors"){
+        if (computerSelection === "rock"){
+            outcome = "你輸惹";
+        }else{
+            outcome = "你贏了";
+        }
+    }else if (playerSelection === "rock") {
+        if (computerSelection === "paper"){
+            outcome = "你輸惹";
+        }else{
+            outcome = "你贏了";
+        }
+    }
+    
+    return outcome;
+    
+}
+
+console.log("玩家出拳:" + playerSelection);
+console.log("電腦出拳:" + computerSelection);
+console.log(playRound(playerSelection, computerSelection));
+
+
